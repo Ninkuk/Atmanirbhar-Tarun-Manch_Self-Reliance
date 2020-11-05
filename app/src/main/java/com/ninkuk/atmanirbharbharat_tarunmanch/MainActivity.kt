f5670029d0f17edf2c96436b2b64aa2ac47df88d
ninkuk <ninadk03@gmail.com>
2020-11-05 13:54:32 -0700

    Implemented navigation component and set up the bottom nav.
    Signed-off-by: ninkuk <ninadk03@gmail.com>

--- app/src/main/java/com/ninkuk/atmanirbharbharat_tarunmanch/MainActivity.kt
+++ app/src/main/java/com/ninkuk/atmanirbharbharat_tarunmanch/MainActivity.kt
@@ -2,10 +2,17 @@ package com.ninkuk.atmanirbharbharat_tarunmanch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        bottom_nav_view.setupWithNavController(navController)
+
     }
 }
