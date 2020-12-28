2d8d50e9ba4fc47bd443cf70275c9912646cff6c
Ninad Kulkarni <ninadk03@gmail.com>
2020-12-28 01:30:59 -0700

    Refactored Main Activity and layout
    Signed-off-by: ninkuk <ninadk03@gmail.com>

--- app/src/main/java/com/ninkuk/atmanirbharbharat_tarunmanch/ui/MainActivity.kt
+++ app/src/main/java/com/ninkuk/atmanirbharbharat_tarunmanch/ui/MainActivity.kt
@@ -3,28 +3,38 @@ package com.ninkuk.atmanirbharbharat_tarunmanch.ui
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.ninkuk.atmanirbharbharat_tarunmanch.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
-    private val TAG = "MainActivity"
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
         val navController = findNavController(R.id.nav_host_fragment)
         bottom_nav_view.setupWithNavController(navController)
+    }
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.categoryListFragment -> bottom_nav_view.visibility = View.GONE
-                R.id.businessPageFragment -> bottom_nav_view.visibility = View.GONE
-                else -> bottom_nav_view.visibility = View.VISIBLE
-            }
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        when (destination.id) {
+            R.id.categoryListFragment -> bottom_nav_view.visibility = View.GONE
+            R.id.businessPageFragment -> bottom_nav_view.visibility = View.GONE
+            R.id.submissionFormFragment -> bottom_nav_view.visibility = View.GONE
+            else -> bottom_nav_view.visibility = View.VISIBLE
         }
     }
+
 }
